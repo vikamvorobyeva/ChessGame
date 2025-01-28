c877262b7df5f7acf6f4b8a4a5bd4598688e5518
qwertyuioppoiuytrewq <aradevre@gmail.com>
2025-01-28 20:35:47 +0300

    1

--- app/models.py
+++ app/models.py
@@ -13,3 +13,14 @@ class User(Base):
     game_count = Column(Integer, default=0)  # Количество игр
     created_at = Column(DateTime, nullable=False)  # Время создания пользователя
     user_rate = Column(Integer, default=0)  # Рейтинг пользователя
+
+
+class Rooms(Base):
+    __tablename__ = "Rooms"  # Название таблицы в БД
+
+    id = Column(Integer, primary_key=True, index=True)  # Уникальный идентификатор
+    is_chess = Column(Boolean, nullable=False)  # Логическое значение (шахматы или шашки)
+    color = Column(String, nullable=False)  # Цвет (например, "черный" или "белый")
+    room_code = Column(String, unique=True, nullable=False)  # Уникальный код комнаты
+    room_password = Column(String, nullable=True)  # Пароль комнаты (может быть NULL)
+
